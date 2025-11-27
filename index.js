360a4436d9d50b01da96bd997125ff0e89fc2557
Paolo Insogna <paolo@cowtech.it>
2025-11-27 04:47:26 +0100

    fix: Correctly sort type only imports.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Do not move this file into src. Typescript resolution will not work in packages.
-
-import JSON5 from 'json5'
 import { readFileSync } from 'node:fs'
 import { resolve } from 'node:path'
+import JSON5 from 'json5'
+
+// Do not move this file into src. Typescript resolution will not work in packages.
 
 export const config = JSON5.parse(readFileSync(resolve(import.meta.dirname, './tsconfig.json'), 'utf8'))
 export default config
